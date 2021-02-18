75d703b4076109e60c9a7b235a7f33a9bbe86ee0
unknown <you@example.com>
2021-02-18 21:22:49 +0600

    made a ts logo downloader

---
+++ index.js
@@ -0,0 +1,4 @@
+document.getElementById("get").addEventListener("click", function () {
+    document.getElementById("home-page").style.display = "none";
+    document.getElementById("logo-page").style.display = "block";
+});
